a2a92a0502b2335f7053dc3fd7cd60306040f1d1
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2019-09-12 00:02:03 +0530

    formated: convert_raw_data_to_tfrec file

--- convert_raw_data_to_tfrecord.py
+++ convert_raw_data_to_tfrecord.py
@@ -1,5 +1,6 @@
 # import path for path functions
 from path import Path
+
 # import tensorflow
 import tensorflow as tf
 
@@ -8,19 +9,19 @@ import random
 
 
 # load the root path
-root_path = Path('Paleographers_data\dataset_icr\dataset')
+root_path = Path("Paleographers_data\dataset_icr\dataset")
 
 # convert the root path into a list of strings
-all_images_path = [str(path) for path in root_path.glob('*\*')]
+all_images_path = [str(path) for path in root_path.glob("*\*")]
 
 # create a randomized images
 random.shuffle(all_images_path)
 
 # get the list of all the dirs
-all_root_labels = [str(path.name) for path in root_path.glob('*') if path.isdir()]
+all_root_labels = [str(path.name) for path in root_path.glob("*") if path.isdir()]
 
 # convert the lables into a dict
-root_labels = dict((c,i) for i,c in enumerate(all_root_labels))
+root_labels = dict((c, i) for i, c in enumerate(all_root_labels))
 
 # extract all the classes for all the images
 all_images_labels = [root_labels[Path(image).parent.name] for image in all_images_path]
@@ -33,6 +34,7 @@ def process_image(image_path):
     decode_image = tf.image.decode_image(raw_image)
 
     return decode_image / 255
+
 
 # make a dataset of all the labels
 image_dataset = tf.data.Dataset.from_tensor_slices(all_images_path)
