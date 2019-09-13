4251a47a6d24277c76fcbe2df7dbc86097817c97
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2019-09-13 13:52:00 +0530

    formated: load imagesds

--- load_imageds.py
+++ load_imageds.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 # import random
 import random
 
+
 class LoadData(object):
     """
     A data loader class for loading images from the respective dirs
@@ -21,24 +22,29 @@ class LoadData(object):
     def __load_labels(self):
 
         # path to all the images in list of str
-        self.__all_images_path = [str(path) for path in self.path_to_dir.glob('*/*')]
+        self.__all_images_path = [str(path) for path in self.path_to_dir.glob("*/*")]
 
         # shuffle the images to add variance
         random.shuffle(self.__all_images_path)
 
         # get the list of all the dirs
-        all_root_labels = [str(path.name) for path in self.path_to_dir.glob('*') if path.isdir()]
+        all_root_labels = [
+            str(path.name) for path in self.path_to_dir.glob("*") if path.isdir()
+        ]
 
         # design the dict of the labels
-        self.root_labels = dict((c,i) for i, c in enumerate(all_root_labels))
+        self.root_labels = dict((c, i) for i, c in enumerate(all_root_labels))
 
         # add the labels for all the images
-        all_images_labels = [self.root_labels[Path(image).parent.name] for image in self.__all_images_path]
+        all_images_labels = [
+            self.root_labels[Path(image).parent.name]
+            for image in self.__all_images_path
+        ]
 
         return all_images_labels
 
     # function to make process the images
-    def __process_image(self,image_path):
+    def __process_image(self, image_path):
         # read image into a raw format
         raw_image = tf.io.read_file(image_path)
         # decode the image
@@ -49,9 +55,9 @@ class LoadData(object):
 
     # make a dataset emitter function
     def emit_dataset(self):
-        # make a dataset for the labels 
+        # make a dataset for the labels
         labels_dataset = tf.data.Dataset.from_tensor_slices(self.__load_labels())
-        
+
         # develop an image dataset
         image_dataset = tf.data.Dataset.from_tensor_slices(self.__all_images_path)
 
